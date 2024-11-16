5b1cc1dbb6afafd4453f37d08774a9f19191bd28
Ege Ayan <egeayan2478@gmail.com>
2024-11-16 21:26:59 +0300

    server creating endpoint

---
+++ lib/current-profile.ts
@@ -0,0 +1,20 @@
+import { auth } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+
+export const currentProfile = async () => {
+  const authContent = await auth();
+  const { userId } = authContent;
+
+  if (!userId) {
+    return null;
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  return profile;
+};
