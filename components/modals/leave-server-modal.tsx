1e984709755716576ff860ae2f0f298d355f4f61
Ege Ayan <egeayan2478@gmail.com>
2024-11-30 20:15:17 +0300

    leave server endpoint implemented

--- components/modals/leave-server-modal.tsx
+++ components/modals/leave-server-modal.tsx
@@ -12,15 +12,30 @@ import {
 import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export const LeaveServerModal = () => {
   const { isOpen, onClose, type, data } = useModal();
 
   const isModalOpen = isOpen && type === "leaveServer";
   const { server } = data;
+  const router = useRouter();
 
   const [isLoading, setIsLoading] = useState(false);
 
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      await axios.patch(`/api/servers/${server?.id}/leave`);
+      onClose();
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -40,7 +55,7 @@ export const LeaveServerModal = () => {
             <Button disabled={isLoading} variant="ghost" onClick={onClose}>
               Cancel
             </Button>
-            <Button disabled={isLoading} variant="primary" onClick={() => {}}>
+            <Button disabled={isLoading} variant="primary" onClick={onClick}>
               Confirm
             </Button>
           </div>
